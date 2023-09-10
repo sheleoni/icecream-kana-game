5d35ad4c57a27d9f7bdaf6f6b2be7c99107d2c4d
Sheleoni <sheleoni@gmail.com>
2023-09-10 22:38:07 +0900

    fix: production mode fetch error

--- src/app/controllers/getUserIceCream.ts
+++ src/app/controllers/getUserIceCream.ts
@@ -4,7 +4,6 @@ export default async function getUserIceCream () {
     try {
         const response = await fetch('http://localhost:3000/api/user/userCollection', {
             method: 'GET',
-            headers: headers()
         });
         const iceCream = await response.json();
         return iceCream;
