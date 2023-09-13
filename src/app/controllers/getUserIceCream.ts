601cda76ac443514640857143179ca03712db6d0
Sheleoni <sheleoni@gmail.com>
2023-09-14 01:10:38 +0900

    fix: fetch DB data in server instead of route handler in /api

--- src/app/controllers/getUserIceCream.ts
+++ src/app/controllers/getUserIceCream.ts
@@ -1,23 +1,41 @@
 import {headers} from "next/headers";
+import {connectDB} from "@/utils/database";
+import {getServerSession} from "next-auth";
+import User from "@/models/user";
+import IceCream from "@/models/iceCream";
+import {NextResponse} from "next/server";
 
 export default async function getUserIceCream () {
-    try {
-        const response = await fetch('http://localhost:3000/api/user/userCollection', {
-            method: 'GET',
-            headers: headers(), // todo: solve header problem
-            // headers: {
-            //     'Cache-Control': 'no-cache',
-            // },
+
+    await connectDB();
+    const session = await getServerSession(); // the headers must be passed in order to get the session in getServerSession
+    const userEmail = session?.user?.email;
+
+    let unlockedIceCream = [];
+
+    if (userEmail) {
+        const user = await User.findOne({
+            email: userEmail
         });
-        // console.log(headers(), `headers()!!`)
-        // const iceCream = await response.json();
-        // return iceCream;
-        console.log('Headers:', headers());
-        const responseText = await response.text();
-        console.log('Response text:', responseText);
-        return JSON.parse(responseText);
-    } catch (error) {
-        console.log(error, `getUserIceCream error`)
-        console.error(`An error occurred while fetching ice cream: ${error}`);
+        if (user) {
+            unlockedIceCream = user.unlockedIceCreams
+            console.log(`unlockedIceCream array for ${userEmail} is ${unlockedIceCream}`)
+
+            // Fetch ice cream details for each unlocked ice cream
+            const iceCreamIds = user.unlockedIceCreams.map((item: any) => item.iceCream);
+            const iceCreamDetails = await IceCream.find({'_id': {$in: iceCreamIds}});
+
+            // Map the IDs back to their full details
+            const userIceCreamCollection = user.unlockedIceCreams.map((item: any) => {
+                const details = iceCreamDetails.find(detail => detail._id.toString() === item.iceCream.toString());
+                return {
+                    iceCream: details.name,
+                    imageURL: details.imageURL,
+                    quantity: item.quantity,
+                };
+            });
+
+            return userIceCreamCollection;
+        }
     }
 }
