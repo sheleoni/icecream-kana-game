4fde57b015d6edf7d1afd42bf739ee3562d50b21
Sheleoni <sheleoni@gmail.com>
2023-09-19 23:33:51 +0900

    feat: render checkbox list with toggle function

---
+++ src/app/play/components/QuestionFilter/FilterOptions.js
@@ -0,0 +1,39 @@
+const FilterOptions = {
+    // all options are initialized to false on initial load
+    hiragana: {
+        'あ': false,
+        'か': false,
+        'さ': false,
+        'た': false,
+        'な': false,
+        'は': true,
+        'ま': false,
+        'や': false,
+        'ら': false,
+        'わ': false,
+        'が': false,
+        'ざ': false,
+        'だ': false,
+        'ば': false,
+        'ぱ': false
+    },
+    katakana: {
+        'ア': false,
+        'カ': false,
+        'サ': false,
+        'タ': false,
+        'ナ': false,
+        'ハ': false,
+        'マ': false,
+        'ヤ': false,
+        'ラ': false,
+        'ワ': false,
+        'ガ': false,
+        'ザ': false,
+        'ダ': false,
+        'バ': false,
+        'パ': false
+    }
+}
+
+export default FilterOptions;
--- src/app/play/components/QuestionFilter/QuestionFilter.tsx
+++ src/app/play/components/QuestionFilter/QuestionFilter.tsx
@@ -1,18 +1,84 @@
+'use client';
+
 import * as Checkbox from "@radix-ui/react-checkbox";
-import styles from './QuestionFilter.module.css'
+import styles from './QuestionFilter.module.css';
+import {useState} from "react";
+import FilterOptions from "@/app/play/components/QuestionFilter/FilterOptions";
+
 const QuestionFilter = () => {
+
+    const HIRAGANA_MODE = "hiragana";
+    const KATAKANA_MODE = "katakana";
+    const [filterMode, setFilterMode] = useState<string>(HIRAGANA_MODE); // toggle between Hiragana or Katakana mode
+
+    const switchMode = (filterMode: string): void => {
+        console.log("switch modes")
+        let newMode;
+        // toggles between Hiragana and Katakana modes
+        if (filterMode === HIRAGANA_MODE) {
+            newMode = KATAKANA_MODE;
+            setFilterMode(newMode)
+        } else {
+            newMode = HIRAGANA_MODE;
+            setFilterMode(newMode)
+        }
+    };
+
+    const filterEntries = Object.entries(FilterOptions[filterMode]);
+    /* transforms, for example,
+
+        {
+        'あ': false,
+        'か': false,
+        'さ': false
+        }
+
+        to
+
+        [
+            [”あ", "false"],
+            [”か", "false"],
+            ["さ", "false"]
+        ]
+
+        and so on...
+     */
+
+
     return (
         <>
-            <li className={styles.checkboxContainer}>
-                <Checkbox.Root className={`${styles.CheckboxRoot} ${styles.button}`} id={"foo"}>
-                    <Checkbox.Indicator>
-                       ✔
-                    </Checkbox.Indicator>
-                </Checkbox.Root>
-                <label className={styles.Label} htmlFor={"foo"}>
-                    HELLO
-                </label>
-            </li>
+            <aside>
+                current mode: { filterMode }
+                <button onClick={() => switchMode(filterMode)}>
+                    Switch mode
+                </button>
+                <li className={styles.checkboxContainer}>
+                    { filterEntries.map(([character, isChecked]) => {
+                        return (
+                                <ul key={character}>
+                                    <Checkbox.Root className={`${styles.CheckboxRoot} ${styles.button}`} id={character}>
+                                        <Checkbox.Indicator>
+                                            ✔
+                                        </Checkbox.Indicator>
+                                    </Checkbox.Root>
+                                    <label className={styles.Label} htmlFor={character}>
+                                        {character}
+                                    </label>
+                                </ul>
+                        )
+                    })}
+                    <ul>
+                    <Checkbox.Root className={`${styles.CheckboxRoot} ${styles.button}`} id={"foo"}>
+                        <Checkbox.Indicator>
+                           ✔
+                        </Checkbox.Indicator>
+                    </Checkbox.Root>
+                    <label className={styles.Label} htmlFor={"foo"}>
+                        HELLO
+                    </label>
+                    </ul>
+                </li>
+            </aside>
         </>
     )
 }
