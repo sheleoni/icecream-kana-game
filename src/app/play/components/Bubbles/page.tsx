3e3a9d3401d11a8ade2260768fe55b204a16d244
Sheleoni <sheleoni@gmail.com>
2023-09-24 10:14:36 +0900

    feat: refined bubble css

--- src/app/play/components/Bubbles/page.tsx
+++ src/app/play/components/Bubbles/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./Bubbles.module.css";
 
 const Bubbles = () => {
-    const tempChoiceArray = ['a', 'tsu', 'u', 'e', 'o'];
+    const tempChoiceArray = ['a', 'i', 'u', 'e', 'o'];
     return (
         <>
             <aside className={styles.bubbleAreaContainer}>
@@ -11,6 +11,7 @@ const Bubbles = () => {
                 { tempChoiceArray.map((choice) => {
                     return (
                         <li key={choice} className={styles.bubbleContainer}>
+                        {/*  todo: consider getting length of choice (e.g. 'i' = 1; 'tsu' = 3 and adjust size with different CSS classes  */}
                         <Image
                              src="https://res.cloudinary.com/dd1dw34dc/image/upload/v1676767326/hiragana_game/Bubble_background_opudxy.gif"
                              alt=""
