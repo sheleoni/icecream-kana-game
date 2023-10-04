384254e19379c10c1b4eb438b5dd7a7421581e2c
Sheleoni <sheleoni@gmail.com>
2023-10-05 01:42:47 +0900

    feat: increase tide level on correct answer

--- src/app/play/components/Hexagons/page.tsx
+++ src/app/play/components/Hexagons/page.tsx
@@ -1,29 +1,47 @@
 import Hexagon from "@/app/play/components/Hexagons/Hexagon/page";
 import styles from './Hexagons.module.css';
+import rowKana from "@/app/play/data/rowKana";
+import React from "react";
 
-const Hexagons = () => {
-    const tempHexagonTideLevelsObject = {
-        'あ': 3,
-        'い': 4,
-        'う': 1,
-        'え': 2,
-        'お': 5,
+type Props = {
+    currentQuestionLetter: string,
+    tideLevel: object,
+}
+const Hexagons = ({ currentQuestionLetter, tideLevel }: Props) => {
+    const findRowByKana = (kana: string, rowKana: {[key: string]: string[]}): string | null => { // todo: refactor duplicate function here (also in <Bubbles /> component
+        // returns the row of the single inputted kana character (e.g. input 「こ」 → outputs 「か行」 since こ belongs to か行)
+        for (const [row, kanaArray] of Object.entries(rowKana)) {
+            // Object.entries(rowKana) looks like this:
+            // [    ↓ row    ↓ kanaArray
+            //     ['あ行', ['あ', 'い', 'う', 'え', 'お']],
+            //     ['か行', ['か', 'き', 'く', 'け', 'こ']],
+            //     and so on...
+            // ]
+            if (kanaArray.includes(kana)) {
+                return row;
+            }
+        }
+        return null;  // or throw an error, or return a default value, etc.
     }
-    const tempHexagonTideLevelsArray = Object.entries(
-        tempHexagonTideLevelsObject
-    )
-    // ↑ transform tide level object to [['あ', 3], ['い', 4]] and so on, for easier mapping in JSX.
+    const currentQuestionRow: string = `${findRowByKana(currentQuestionLetter, rowKana)}`; // todo: refactor duplicate variable here (also in <Bubbles /> component
+    const hexagonCharacters: string[] = rowKana[currentQuestionRow as keyof typeof rowKana];
+
+    const handleClickHexagon = () => {
+    // todo: handle click hexagon
+        // if hexagon tide level = 5, reset hexagon level
+        // add new ice cream scoop
+    }
 
     return (
         <>
             <ol className={styles.hexagonsContainer}>
             {
-                tempHexagonTideLevelsArray.map(([character, tideLevel]: [string, number]) => {
+                hexagonCharacters?.map((character: string) => {
                     return (
                             <li key={character} className={styles.hexagon}>
                                 <Hexagon
                                     character={character}
-                                    tideLevel={tideLevel} />
+                                    tideLevel={tideLevel[character as keyof typeof tideLevel]} />
                             </li>)
                 })
             }
