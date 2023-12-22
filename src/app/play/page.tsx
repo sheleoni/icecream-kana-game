cd586db175e29fb7cbee5ac67849fcb68a960df4
Sheleoni <sheleoni@gmail.com>
2023-12-23 03:49:33 +0900

    feat: fetch kana score breakdown from DB and post to DB on submit

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -4,6 +4,7 @@ import {getServerSession} from "next-auth";
 import Link from "next/link";
 import React from "react";
 import getUserTotalScore from "@/app/controllers/getUserTotalScore";
+import getUserKanaScores from "@/app/controllers/getUserKanaScores";
 
 const GamePage = async () => {
     const session = await getServerSession();
@@ -29,7 +30,6 @@ const GamePage = async () => {
     let userTideLevelData = await getUserTideLevel();
     const stringifiedUserTideLevel = JSON.stringify(userTideLevelData);
     const userTideLevelArray = JSON.parse(stringifiedUserTideLevel);
-
     if (userTideLevelData.length === 0) {
         userTideLevelData = null;
     }
@@ -38,12 +38,24 @@ const GamePage = async () => {
         return accumulator;
     }, {});
 
+    // load kanaScores from DB
+    let userKanaScoresData = await getUserKanaScores();
+    const stringifiedUserKanaScoresData = JSON.stringify(userKanaScoresData);
+    const userKanaScoresArray = JSON.parse(stringifiedUserKanaScoresData);
+    if (userKanaScoresArray.length === 0) {
+        userKanaScoresData = null;
+    }
+    const kanaScores = userKanaScoresArray.reduce((accumulator: any, { kana, score }: any) => { // todo: replace :any type
+        accumulator[kana] = score;
+        return accumulator;
+    }, {});
+
     // load user total score from DB
     let userTotalScoreData = await getUserTotalScore();
     return (
         <>
             <h1>Hello game page, this is server-rendered</h1>
-            <PlayArea userTideLevel={tideLevel} userTotalScoreData={userTotalScoreData}/>
+            <PlayArea userTideLevel={tideLevel} userTotalScoreData={userTotalScoreData} userKanaScores={kanaScores}/>
         {/*    todo: cater to new users - if use does not have tideLevel stored, use the default tideLevel.js on the client */}
         </>
     );
