eedd631de5f2cfc351289df27f053761e3590a13
Sheleoni <sheleoni@gmail.com>
2023-12-26 00:56:45 +0900

    feat: render ice cream on /collection

--- src/app/user/collection/page.tsx
+++ src/app/user/collection/page.tsx
@@ -5,10 +5,17 @@ import {getServerSession} from "next-auth";
 import getUserIceCream from "@/app/controllers/getUserIceCream";
 import styles from './page.module.css';
 import getUserTideLevel from "@/app/controllers/getUserTideLevel";
+import getUserIceCreamCollection from "@/app/controllers/getUserIceCreamCollection";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic"
+
+// todo: get collection dat ahere
 const CollectionOverview = async () => {
     const session = await getServerSession();
+    const userIceCreamCollectionNEW = await getUserIceCreamCollection();
+    console.log(userIceCreamCollectionNEW, 'USER ICE CREAM NEWWWWW');
+    console.log(userIceCreamCollectionNEW.length, 'LENGTH OF COLLECTION')
     const userTideLevel = await getUserTideLevel();
     console.log(userTideLevel, 'USERRR TIDE LEVELLLLL')
     const userIceCreamCollection = await getUserIceCream();
@@ -17,11 +24,18 @@ const CollectionOverview = async () => {
         <>
             Your ice-cream collection:
             {
-                userIceCreamCollection?.map((flavour: any, index: any): any => {
+                userIceCreamCollection?.map((flavour: any, index: any): any => { // todo: refine any type
                     return (
                         <p key={index} className={styles.iceCreamItem}>
                             {flavour.iceCream} you have {flavour.quantity}
                         </p>
+                    )
+                })
+            }
+            {
+                userIceCreamCollectionNEW?.map((iceCream: any, index: any): any => { // todo: refine any type
+                    return (
+                        <Image alt={iceCream.name} key={index} src={iceCream.imgURL}  width={140} height={150}/>
                     )
                 })
             }
